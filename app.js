e53179692946c012993b153caf92a970b7b4b3ae
Osama Ahmed Qureshi <osamaxqureshi@protonmail.com>
2022-12-29 03:03:58 +0500

    Applied Bootstrap CSS to ejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { request } = require("http");
 const app = express();
 app.use(express.urlencoded());
 const https = require("https");
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 // for random port on server
 app.use(express.static("Public"));
@@ -15,10 +16,19 @@ app.get("/", function (req, res) {
 	var today = new Date();
 	var currentDay = today.getDate();
 
+	var options = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	};
+
+	var day = today.toLocaleDateString("en-GB", options);
+
 	// to get Day of the week
-	const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-	const d = new Date();
-	let day = weekday[d.getDay()];
+	// const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	// const d = new Date();
+	// let day = weekday[d.getDay()];
 
 	//to get suggested day according to the rules
 	// if (currentDay === 6 || currentDay === 0) {
@@ -28,8 +38,14 @@ app.get("/", function (req, res) {
 	// 	day = "Weekday";
 	// }
 
-	res.render("list", { kindofDay: day })
+	res.render("list", { kindofDay: day, newListItems: items })
 });
+
+app.post("/", function (req, res) {
+	var item = req.body.newItem;
+	items.push(item);
+	res.redirect("/");
+})
 
 app.listen(process.env.PORT || 3000, function () {
 	console.log("Server Has Started @ 3000 Port");
